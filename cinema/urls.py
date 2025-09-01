e693f0574bf9b3802dc63e7e120ac4e49f34679a
Hetmarum <kpxmedet@gmail.com>
2025-09-01 17:46:10 +0300

    adding patch and path fixes

--- cinema/urls.py
+++ cinema/urls.py
@@ -13,17 +13,13 @@ from cinema.views import (
 )
 
 
-cinema_hall_list = CinemaHallViewSet.as_view(
-    actions={"get": "list", "post": "create"}
-)
-cinema_hall_detail = CinemaHallViewSet.as_view(
-    actions={
-        "get": "retrieve",
-        "put": "update",
-        "patch": "partial_update",
-        "delete": "destroy",
-    }
-)
+cinema_hall_list = CinemaHallViewSet.as_view({"get": "list", "post": "create"})
+cinema_hall_detail = CinemaHallViewSet.as_view({
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy",
+})
 
 router = routers.DefaultRouter()
 
@@ -35,8 +31,8 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinema-halls/", cinema_hall_list, name="cinema-hall-list"),
-    path("cinema-halls/<int:pk>/",
+    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
+    path("cinema_halls/<int:pk>/",
          cinema_hall_detail,
          name="cinema-hall-detail"
          ),
